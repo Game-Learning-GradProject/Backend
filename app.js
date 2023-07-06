f28d9fcbda41b05ecbfb17e5b72caec6abab19e8
Ahmed Elsellamy <ahmedelsellamy@gmail.com>
2023-07-06 13:53:55 +0300

    adding react build for production

--- app.js
+++ app.js
@@ -10,21 +10,25 @@ const FSE = require("./routes/DaTa");
 const data = require("./routes/dataRouter");
 const Task = require("./routes/Task");
 const feedback = require("./routes/feedbackRouter");
-const routes = require('./routes/routes');
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin",  "http://localhost:5173");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// })
-app.use('/', routes);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin",  "http://localhost:8080");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+})
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+});
+
 app.use("/image", express.static(path.join("Image")));
 app.use("/StudentProfilePic", express.static(path.join("StudentProfilePic")));
 app.use("/Profile", express.static(path.join("Profile")));
