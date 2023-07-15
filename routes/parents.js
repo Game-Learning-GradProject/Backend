d07a97d8498917f9f7d504326aaf03c5e8552770
Abdullah Saeed <abdullahhamouda056@gmail.com>
2023-07-15 22:42:42 +0300

    post /updateParentInfo/:id

--- routes/parents.js
+++ routes/parents.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const { execMap } = require('nodemon/lib/config/defaults');
+const express = require("express");
+const { execMap } = require("nodemon/lib/config/defaults");
 const router = express.Router();
 
-const parentController = require('../controller/parentController');
-const { updateMany } = require('../models/parentdb');
+const parentController = require("../controller/parentController");
+const { updateMany } = require("../models/parentdb");
 
+router.post("/create", parentController.SignUp);
 
-
-
-
-router.post('/create', parentController.SignUp );
-
-router.post('/login', parentController.SignIn);
-router.patch('/updateParentInfo/:id', parentController.upload.single('image'),parentController.updateParentInfo);
-router.delete('/deleteAccount/:id',parentController.deleteAccount);
-router.post('/forgot-password', parentController.forgotPassword);
-router.post('/reset-password/:token', parentController.resetPassword);
-
-
-
+router.post("/login", parentController.SignIn);
+router.post(
+  "/updateParentInfo/:id",
+  parentController.upload.single("image"),
+  parentController.updateParentInfo
+);
+router.delete("/deleteAccount/:id", parentController.deleteAccount);
+router.post("/forgot-password", parentController.forgotPassword);
+router.post("/reset-password/:token", parentController.resetPassword);
 
 module.exports = router;
